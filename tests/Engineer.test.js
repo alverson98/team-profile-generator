ad5f97082847e13f63830f87fdacbc08a486d677
Hannah Alverson <alverson98@yahoo.com>
2022-08-30 20:15:10 -0600

    imported engineer class and created tests for all of the methods

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,13 +1,41 @@
+const Engineer = require("../lib/Engineer");
+
 describe("Engineer class", () => {
-  it("", () => {});
+  it("should create an engineer object including name, id, email, and github", () => {
+    const engineer = new Engineer(
+      "Hannah",
+      10,
+      "hannah@example.com",
+      "githubUsername"
+    );
 
-  describe("getName", () => {});
+    expect(engineer.name).toEqual("Hannah");
+    expect(engineer.id).toEqual(10);
+    expect(engineer.email).toEqual("hannah@example.com");
+    expect(engineer.github).toEqual("githubUsername");
+    expect(typeof engineer).toEqual("object");
+  });
 
-  describe("getId", () => {});
+  describe("getGithub", () => {
+    it("should get github username from getGithub method", () => {
+      const github = "githubUsername";
+      const engineer = new Engineer("Hannah", 10, "hannah@example.com", github);
 
-  describe("getEmail", () => {});
+      expect(engineer.getGithub()).toEqual(github);
+    });
+  });
 
-  describe("getGithub", () => {});
+  // Overridden to return Engineer
+  describe("getRole", () => {
+    it("should return 'Engineer' from getRole method", () => {
+      const engineer = new Engineer(
+        "Hannah",
+        10,
+        "hannah@example.com",
+        "githubUsername"
+      );
 
-  describe("getRole", () => {}); // Overridden to return Engineer
+      expect(engineer.getRole()).toEqual("Engineer");
+    });
+  });
 });
