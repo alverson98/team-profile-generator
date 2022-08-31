f8a46fabc4b4a9eed9d7f0508ede27343fc844ae
Hannah Alverson <alverson98@yahoo.com>
2022-08-30 19:39:33 -0600

    added tests for remaining methods

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,27 +1,49 @@
 const Employee = require("../lib/Employee");
 
 describe("Employee class", () => {
-  it("creates an employee object that contains name, id, and email", () => {
+  it("should create an employee object that contains name, id, and email", () => {
     const employee = new Employee("Hannah", 10, "hannah@example.com");
 
     expect(employee.name).toEqual("Hannah");
     expect(employee.id).toEqual(10);
     expect(employee.email).toEqual("hannah@example.com");
+    expect(typeof employee).toEqual("object");
   });
 
   describe("getName", () => {
-    it("gets employee's name", () => {});
+    it("should get employee's name from getName method", () => {
+      const name = "Hannah";
+      const employee = new Employee(name, id, email);
+
+      expect(employee.getName()).toEqual(name);
+    });
   });
 
   describe("getId", () => {
-    it("gets employee's id", () => {});
+    it("should get employee's id from getId method", () => {
+      const id = 10;
+      const employee = new Employee("Hannah", id, email);
+
+      expect(employee.getId()).toEqual(id);
+    });
   });
 
   describe("getEmail", () => {
-    it("gets employee's email", () => {});
+    it("should get employee's email from getEmail method", () => {
+      const email = "hannah@example.com";
+      const employee = new Employee("Hannah", 10, email);
+
+      expect(employee.getEmail()).toEqual(email);
+    });
   });
 
+  //Returns 'Employee'
   describe("getRole", () => {
-    it("gets employee's role", () => {});
-  }); //Returns 'Employee'
+    it("should return employee's role as 'Employee'", () => {
+      const role = "Employee";
+      const employee = new Employee("Hannah", 10, "hannah@example.com");
+
+      expect(employee.getRole()).toEqual(role);
+    });
+  });
 });
