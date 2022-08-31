eedfceb200d98603c8803798d696cb1988f9feac
Hannah Alverson <alverson98@yahoo.com>
2022-08-30 20:25:34 -0600

    added additional expected results for the manager class

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -4,6 +4,9 @@ describe("Manager class", () => {
   it("should create a manager object including name, id, email, and officeNumber", () => {
     const manager = new Manager("Hannah", 10, "hannah@example.com", 25);
 
+    expect(manager.name).toEqual("Hannah");
+    expect(manager.id).toEqual(10);
+    expect(manager.email).toEqual("hannah@example.com");
     expect(manager.officeNumber).toEqual(25);
     expect(typeof manager).toEqual("object");
   });
