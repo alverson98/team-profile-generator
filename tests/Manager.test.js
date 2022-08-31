c9ebc3e0ddb335b462b875d2e0f6c483e96a0076
Hannah Alverson <alverson98@yahoo.com>
2022-08-30 19:57:34 -0600

    created tests for each method

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,11 +1,34 @@
+const Manager = require("../lib/Manager");
+const Employee = require("../lib/Employee");
+
 describe("Manager class", () => {
-  it("", () => {});
+  it("should create a manager object including name, id, email, and officeNumber", () => {
+    const manager = new Employee("Hannah", 10, "hannah@example.com", 25);
 
-  describe("getName", () => {});
+    expect(manager.officeNumber).toEqual(25);
+    expect(typeof manager).toEqual("object");
+  });
 
-  describe("getId", () => {});
+  describe("getOfficeNumber", () => {
+    it("should get the manager's office number from getOfficeNumber method", () => {
+      const officeNumber = 25;
+      const manager = new Employee(
+        "Hannah",
+        10,
+        "hannah@example.com",
+        officeNumber
+      );
 
-  describe("getEmail", () => {});
+      expect(manager.getOfficeNumber()).toEqual(officeNumber);
+    });
+  });
 
-  describe("getRole", () => {}); //Overridden to return 'Manager'
+  //Overridden to return 'Manager'
+  describe("getRole", () => {
+    it("should return 'Manager' from getRole method", () => {
+      const manager = new Employee("Hannah", 10, "hannah@example.com", 25);
+
+      expect(manager.getRole()).toEqual("Manager");
+    });
+  });
 });
